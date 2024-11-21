6a6cc78a90bb01af2b39cbb5d9f6af34252d0b68
khalidAlfaleh <444102210@student.ksu.edu.sa>
2024-11-21 17:54:26 +0300

    Update TreeUser.java

--- src/main/java/com/example/project/TreeUser.java
+++ src/main/java/com/example/project/TreeUser.java
@@ -1,32 +1,34 @@
 package com.example.project;
 
 public class TreeUser {
+    private static <T> int counter(BT<T> bt) {
+		 int count = 0;
+	    	if(bt.empty())
+	    		return 0;
+	    	
+	    	if (bt.isLeaf()) {
+	    		
+	    		return 1;
+	    	}
+	    	
+	    	if(bt.find(Relative.LeftChild)) {
+	    	count += counter(bt);
+	    	bt.find(Relative.Parent);
+	    	}
+	    	
+	    	if(bt.find(Relative.RightChild)) {
+	    	count += counter(bt);
+	    	bt.find(Relative.Parent);
+	    	}
+	    	
+	    	return count;
+	    	
+	 }
+	
     public static <T> int countLeaves(BT<T> bt) {
-
-
-
-    	    	int count = 0;
-    	if(bt.empty())
-    		return 0;
     	
-    	if (bt.isLeaf()) {
-    		
-    		return 1;
-    	}
-    	
-    	if(bt.find(Relative.LeftChild)) {
-    	count += countLeaves(bt);
-    	bt.find(Relative.Parent);
-    	}
-    	
-    	if(bt.find(Relative.RightChild)) {
-    	count += countLeaves(bt);
-    	bt.find(Relative.Parent);
-    	}
-    	
-    	return count;
-    	
-	    
+    	    bt.find(Relative.Root);    
+	    	return counter(bt); 	  	   	
         //throw new UnsupportedOperationException("Not supported yet.");
 		// Write the method countLeafs that should return the number of leaf nodes in the tree. A leaf node is a node that has no children.
         // assume the following method exists in the ADT: isLeaf (boolean flag): requires: Binary tree is not empty.
